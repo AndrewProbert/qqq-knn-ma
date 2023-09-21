1fa5246baf0f2c481f57cf91af7b21a60611ca73
AndrewProbert <andrewprobert15@gmail.com>
2023-09-21 16:23:34 +0100

    Working with live data now!!!

--- subscript.php
+++ subscript.php
@@ -5,74 +5,51 @@ ini_set('display_errors', '0');
 
 
 $symbol ='QQQ';
-function getAllStockData($symbol) {
-    // Initialize an array to store all data
-    $allData = [];
 
-    // Set the initial date to today
-    $endDate = date('Y-m-d');
 
-    // Fetch historical data in chunks
-    while (true) {
-        // Calculate the start date for the current chunk (e.g., 1 year ago)
-       // $startDate = date('Y-m-d', strtotime("$endDate 1 year"));
-        $startDate = date('Y-m-d', strtotime("$endDate -5 day"));
+function getYahooFinanceCloseData() {
+    // Specify the URL of the Yahoo Finance API
+    $url = 'https://query1.finance.yahoo.com/v8/finance/chart/qqq?interval=5m&range=60d';
 
-        // Construct the URL with the date range
-        $url = "https://query1.finance.yahoo.com/v8/finance/chart/{$symbol}?interval=5m&period1=" . strtotime($startDate) . "&period2=" . strtotime($endDate);
+    // Fetch the JSON data from the URL
+    $jsonData = file_get_contents($url);
 
-        // Initialize cURL
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Execute the cURL request
-        $response = curl_exec($ch);
-
-        // Check if cURL request was successful
-        if ($response === false) {
-            return null;
-        }
-
-        // Close cURL connection
-        curl_close($ch);
-
-        // Decode the JSON response
-        $data = json_decode($response, true);
-
-        // Extract the desired data (closing price)
-        if (isset($data['chart']['result'][0]['timestamp'])) {
-            $timestamps = $data['chart']['result'][0]['timestamp'];
-            $close = $data['chart']['result'][0]['indicators']['quote'][0]['close'];
-
-            // Combine data into an array of arrays and prepend it to $allData
-            $chunkData = [];
-            for ($i = 0; $i < count($timestamps); $i++) {
-                $chunkData[] = [
-                    'timestamp' => $timestamps[$i],
-                    'close' => $close[$i],
-                ];
-            }
-            $allData = array_merge($chunkData, $allData);
-
-            // Set the new end date for the next chunk
-            $endDate = date('Y-m-d', strtotime("$startDate -1 day"));
-        } else {
-            break; // No more data available
-        }
+    // Check if data retrieval was successful
+    if ($jsonData === false) {
+        return false; // Return false if there was an error fetching data
     }
 
-    return $allData;
+    // Decode the JSON data
+    $data = json_decode($jsonData, true);
+
+    // Check if the JSON decoding was successful
+    if ($data === null || !isset($data['chart']['result'][0]['indicators']['quote'][0]['close'])) {
+        return false; // Return false if JSON decoding or data structure is not as expected
+    }
+
+    // Extract the close prices
+    $closePrices = $data['chart']['result'][0]['indicators']['quote'][0]['close'];
+
+    return $closePrices;
 }
 
-function getLiveStockData($symbol){
-    
+// Usage example
+$closeData = getYahooFinanceCloseData();
+
+if ($closeData !== false) {
+    foreach ($closeData as $closePrice) {
+        echo "Close Price: $closePrice" . PHP_EOL;
+    }
+} else {
+    echo "Failed to retrieve data from Yahoo Finance API." . PHP_EOL;
 }
 
 
-print_r(getAllStockData($symbol));
+
+
+//print_r(getYahooFinanceCloseData());
 // Get the stock data for the symbol
-$stock_data = getAllStockData($symbol);
+$stock_data = getYahooFinanceCloseData();
 
 // Print the data and capture the output
 ob_start();
